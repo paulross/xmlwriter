0845ffc08790dfbcd188b92a6d1ecf034568f9c0
Paul Ross <apaulross@gmail.com>
2017-11-27 13:04:28 +0000

    Initial commit based on the pybind11 example.

---
+++ xmlwriter/cpp/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  xmlwriter
+//
+//  Created by Paul Ross on 27/11/2017.
+//  Copyright © 2017 Paul Ross. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int /* argc */, const char *[] /* argv[] */) {
+    
+    std::cout << "Hello world" << std::endl;
+    
+    std::cout << "Bye, bye!\n";
+    return 0;
+}
