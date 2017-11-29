f8a25e9c6a0ee2ccc1755339726907b76e0d3d8a
Paul Ross <apaulross@gmail.com>
2017-11-29 10:03:42 +0000

    Failed attempt to pass Python file-like object.

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Python XML/HTML/SVG writer implemented in C++
 
 An XML/HTML/SVG writer project built with [pybind11](https://github.com/pybind/pybind11).
 
+Project is based on the [pybind11 example](https://github.com/pybind/python_example).
+
 Installation
 ------------
 
@@ -82,4 +84,35 @@ Test call
 ```python
 import cXmlWrite
 ```
+
+Discoveries
+===========
+
+The original `XmlWrite.XmlStream` took as the first argument a string or file like object. If the former it was treated as the file path to write to. I havn't been able to reproduce this pattern by using a `PyObject*` and deciding within the constructor what to do:
+
+```c
+    py::class_<XmlStream>(m, "XmlStream")
+        .def(py::init<PyObject*, const std::string &, const std::string &, int, bool>());
+```
+
+This complains with:
+
+```python
+>>> s= "test.xml"
+>>> xs = cXmlWrite.XmlStream(s, "utf-8", "", 0, True)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: __init__(): incompatible constructor arguments. The following argument types are supported:
+    1. cXmlWrite.XmlStream(arg0: _object, arg1: str, arg2: str, arg3: int, arg4: bool)
+
+Invoked with: 'test.xml', 'utf-8', '', 0, True
+```
+
+I can not find a way to map Python's file object to an internal C++ stream: http://pybind11.readthedocs.io/en/stable/advanced/pycpp/object.html
+
+Rewrote both Python and C++ code to write to an internal buffer, the caller can retrieve this and write it to file.
+
+
+
+
 
--- xmlwriter/cpp/cXmlWrite.cpp
+++ xmlwriter/cpp/cXmlWrite.cpp
@@ -1,5 +1,73 @@
 #include <pybind11/pybind11.h>
 
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include <list>
+
+class XmlStream {
+public:
+    XmlStream(PyObject *theFout,
+              const std::string &theEnc/* ='utf-8'*/,
+              const std::string &theDtdLocal /* =None */,
+              int theId /* =0 */,
+              bool mustIndent /* =True */) : fd(-1),
+                                            file(NULL),
+                                            encodeing(theEnc),
+                                            dtdLocal(theDtdLocal),
+                                            id(theId),
+                                            mustIndent(mustIndent),
+                                            _inElem(false) {
+        /* If theFout is a Python string then open it.
+         * Otherwise if it is a Python file-like object then get the file descriptor.
+         * Otherwise raise. */
+        if (PyUnicode_Check(theFout)) {
+            file = fopen(PyUnicode_AS_DATA(theFout), "w");
+            if (!file) {
+                throw std::invalid_argument("Can not open file.");
+            }
+        } else {
+            /* Try as a file descriptor. */
+            fd = PyObject_AsFileDescriptor(theFout);
+            if (fd < 0) {
+                throw std::invalid_argument("Can not extract file descriptor from Pyhon file object");
+            }
+        }
+        
+    }
+private:
+    /* One of these must be valid. */
+    int fd; /* For write(). */
+    FILE *file; /* For fwrite. */
+public:
+    std::string encodeing;
+    std::string dtdLocal;
+    int id;
+    bool mustIndent;
+    std::list<std::string> _elemStk;
+    bool _inElem;
+    std::list<bool> _canIndentStk;
+private:
+    size_t _write(const char *buffer, size_t len) {
+        if (file) {
+            return fwrite(buffer, len, 1, file);
+        } else if (fd >= 0) {
+            return write(fd, buffer, len);
+        }
+        assert(0);
+        return 0;
+    }
+
+};
+
+
+struct TestPyObject {
+    TestPyObject(PyObject *pobj) {
+        PyObject_Print(pobj, stdout, 0);
+    }
+};
+
+
 int add(int i, int j) {
     return i + j;
 }
@@ -31,7 +99,12 @@ PYBIND11_MODULE(cXmlWrite, m) {
 
         Some other explanation about the subtract function.
     )pbdoc");
+    
+    py::class_<XmlStream>(m, "XmlStream")
+        .def(py::init<PyObject*, const std::string &, const std::string &, int, bool>());
 
+    py::class_<TestPyObject>(m, "TestPyObject")
+        .def(py::init<PyObject*>());
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
 #else
