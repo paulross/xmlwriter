e18c5f8afb90e47ac5ac2f58d287820b3f269fd6
Paul Ross <apaulross@gmail.com>
2018-04-10 18:37:38 +0100

    Improvements to pydoc2cppdoc.py

---
+++ xmlwriter/py/xmlwriter/Example.py
@@ -0,0 +1,9 @@
+"""
+Example module documentation.
+"""
+
+class Example:
+    """Example class documentation"""
+    def __init__(self, *args, **kwargs):
+        """Construct an Example."""
+        pass
